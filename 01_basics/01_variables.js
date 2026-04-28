7548f0762e60c38542706a3106a3a4f858279568
Manshi Sharma <76405335+manshisharma1372@users.noreply.github.com>
2026-04-28 18:09:58 +0000

    learned let,var,const

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,18 @@
+const accountId=144553
+let accountEmail="manshimannu123.com"
+var accountPassword="12345"
+accountCity="Jaipur"
+
+//accountId=123
+accountEmail='mmmmmm.com'
+accountCity='mathura'
+accountPassword="8888"
+let accountState;
+
+//console.log(accountId)
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState])
+
+/*
+Prefer not to use var
+because of issue in block scope and functional scope
+*/
