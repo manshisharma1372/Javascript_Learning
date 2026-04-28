360d1fb7c9cb4feec357d15931a6d77a084ff8a7
Manshi Sharma <76405335+manshisharma1372@users.noreply.github.com>
2026-04-28 19:07:37 +0000

    conversion of dataytypes

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,35 @@
+let score = 33
+
+console.log(typeof score)
+console.log(typeof(score))
+
+let scoreInString= String(score)
+console.log(typeof scoreInString)
+
+let newScore="222rrr"
+let valueInNum=Number(newScore)
+console.log(valueInNum)
+console.log(typeof(valueInNum))
+
+let anotherScore=null
+let valueofThis=Number(anotherScore)
+console.log(valueofThis)
+console.log(typeof(valueofThis))
+
+
+let checkScore=undefined
+let valueOfCheckScore=Number(checkScore)
+console.log(valueOfCheckScore)
+
+let flag=true
+let convertFlag=Number(flag)
+console.log(convertFlag)
+
+let isLoggedIn=1
+let booleanIsLoggedIn= Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn)
+
+let num='123'
+let snum=String(num)
+console.log(snum)
+console.log(typeof(snum))
