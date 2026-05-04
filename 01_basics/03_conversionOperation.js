35c931e7a1675c297fa6ee2ddd7262e2230436eb
Manshi Sharma <76405335+manshisharma1372@users.noreply.github.com>
2026-05-04 03:44:41 +0000

    Learned data Types and conversion

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -33,3 +33,25 @@ let num='123'
 let snum=String(num)
 console.log(snum)
 console.log(typeof(snum))
+
+
+//---operations---
+
+let val=3
+let negVal=-val
+console.log(negVal)
+
+let str1="hello"
+let str2="mannu"
+let str3=str1+str2
+console.log(str3)
+
+console.log("1"+2)
+console.log(1+"2")
+console.log("1"+"2")
+console.log("1"+2+2)
+console.log(1+2+"2")
+
+let gameCounter=100
+gameCounter++
+console.log(gameCounter)
